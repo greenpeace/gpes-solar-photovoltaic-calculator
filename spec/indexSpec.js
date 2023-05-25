f1439a71e1efb4dea513273c88aa51aee88d0ad9
Osvaldo Gago <opgago@yahoo.com>
2023-05-25 21:38:33 +0200

    New object to test

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,25 +1,27 @@
 /* jshint esversion:6 */
 
+let t1 = new Calculadora(5000, 80, "Aragón", "S");
+
 describe("Asegurarse que los inputs son validos", () => {
 
     it("El importe de la factura de la luz debe ser un número entero positivo", () => {
-        expect(function() { calc.consumo_anual = -12; }).toThrow();
-        expect(function() { calc.consumo_anual = "A"; }).toThrow();
+        expect(function() { t1.consumo_anual = -12; }).toThrow();
+        expect(function() { t1.consumo_anual = "A"; }).toThrow();
     });
 
     it("Porcentaje tiene que ser un número entero entre 0 y 100", () => {
-        expect(function() { calc.porcentaje_consumo = -5; }).toThrow();
-        expect(function() { calc.porcentaje_consumo = -1; }).toThrow();
-        expect(function() { calc.porcentaje_consumo = 101; }).toThrow();
-        expect(function() { calc.porcentaje_consumo = "Z"; }).toThrow();
+        expect(function() { t1.porcentaje_consumo = -5; }).toThrow();
+        expect(function() { t1.porcentaje_consumo = -1; }).toThrow();
+        expect(function() { t1.porcentaje_consumo = 101; }).toThrow();
+        expect(function() { t1.porcentaje_consumo = "Z"; }).toThrow();
     });
 
     it("CCA tiene que ser válida", () => {
-        expect(function() { calc.comunidad_autonoma = "España"; }).toThrow();
+        expect(function() { t1.comunidad_autonoma = "España"; }).toThrow();
     });
 
     it("Orientación tiene que ser válida", () => {
-        expect(function() { calc.orientacion_tejado = "O"; }).toThrow();
+        expect(function() { t1.orientacion_tejado = "O"; }).toThrow();
     });
 
 });
@@ -27,9 +29,7 @@ describe("Asegurarse que los inputs son validos", () => {
 describe("Energia a cubrir", () => {
 
     it("Cálculo de energía a cubrir", ()=> {
-        calc.consumo_anual = 5000;
-        calc.porcentaje_consumo = 80;
-        expect(calc.energia_a_cubrir()).toBe(4000);
+        expect(t1.energia_a_cubrir()).toBe(4000);
     });
 
 });
@@ -37,7 +37,7 @@ describe("Energia a cubrir", () => {
 describe("Producción de un pannel", () => {
 
     it("Producción de un pannel, média nacional", ()=> {
-        expect(calc.produccion_de_un_panel()).toBe(1500);
+        expect(t1.produccion_de_un_panel()).toBe(1500);
     });
 
 });
@@ -45,7 +45,7 @@ describe("Producción de un pannel", () => {
 describe("Potencia necesaria a instalar(kWp)", () => {
 
     it("Potencia necesaria a instalar(kWp)", ()=> {
-        expect(calc.potencia_necesaria_a_instalar()).toBeCloseTo(2.67,1);
+        expect(t1.potencia_necesaria_a_instalar()).toBeCloseTo(2.67,1);
     });
 
 });
@@ -53,7 +53,7 @@ describe("Potencia necesaria a instalar(kWp)", () => {
 describe("Coste de instalación", () => {
 
     it("Coste de instalación", ()=> {
-        expect(calc.coste_de_instalacion()).toBe(4800);
+        expect(t1.coste_de_instalacion()).toBe(4800);
     });
 
 });
@@ -62,7 +62,7 @@ describe("Coste de instalación", () => {
 describe("Ahorro anual esperado", () => {
 
     it("Ahorro anual esperado", ()=> {
-        expect(calc.ahorro_anual_esperado()).toBe(800);
+        expect(t1.ahorro_anual_esperado()).toBe(800);
     });
 
 });
@@ -71,7 +71,7 @@ describe("Ahorro anual esperado", () => {
 describe("Años de amortizacion", () => {
 
     it("Años de amortizacion", ()=> {
-        expect(calc.anos_amortizacion()).toBe(6);
+        expect(t1.anos_amortizacion()).toBe(6);
     });
 
 });
