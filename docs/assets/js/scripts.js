c1141e7f6b9e0cbfde864884d9f9690c2fcfedf7
Osvaldo Gago <opgago@yahoo.com>
2023-07-15 09:14:25 +0100

    Some interactions

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,16 +1,30 @@
-const animateCSS = (element, animation, prefix = 'animate__') =>
-new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    const node = document.querySelector(element);
-    node.classList.add(`${prefix}animated`, animationName);
-    function handleAnimationEnd(event) {
-        event.stopPropagation();
-        node.classList.remove(`${prefix}animated`, animationName);
-        resolve('Animation ended');
-    }
-    node.addEventListener('animationend', handleAnimationEnd, { once: true });
-});
+/* eslint-disable no-unused-vars */
 
+/**
+ * Animate.css animation
+ * @param {string}} element
+ * @param {string} animation
+ * @param {string} [prefix='animate__']
+ * @returns {*}
+ */
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = document.querySelector(element);
+        node.classList.add(`${prefix}animated`, animationName);
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
+
+/**
+ * Enter animation
+ * @param {string} element
+ * @param {string} animation
+ */
 const enter = function (element, animation) {
     const el = document.querySelector(element);
     if (el.hasAttribute("hidden")) {
@@ -20,6 +34,12 @@ const enter = function (element, animation) {
         });
     }
 };
+
+/**
+ * Leave animation
+ * @param {string} element
+ * @param {string} animation
+ */
 const leave = function (element, animation) {
     const el = document.querySelector(element);
     if (!el.hasAttribute("hidden")) {
@@ -28,4 +48,16 @@ const leave = function (element, animation) {
             el.setAttribute("hidden", "hidden");
         });
     }
+};
+
+/**
+ * Scroll to a specific id
+ * @param {string} id 
+ */
+const flyTo = function (id, delay = 200) {
+    setTimeout(function () {
+        document.getElementById(id).scrollIntoView({
+            behavior: "smooth"
+        });
+    }, delay);
 };
